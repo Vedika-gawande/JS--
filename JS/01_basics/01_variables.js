00c7ad4f786af28145818ce92339c9d00f93a71c
Vedika Gawande <vedikagawandde91@gmail.com>
2026-01-21 17:39:37 +0530

    variables in js

---
+++ JS/01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountId=141414
+let accountName="vedika"
+var accountPassword=87644
+accountCity="Singapore"
+let account;
+//accountId=212121
+// cant change the value of constant once it is declared
+console.log(accountId)
+console.table([accountId,accountName,accountPassword,accountCity,account])
+//if we dont assign the value then it is undefined
+//will never use var because of it scope problem 
