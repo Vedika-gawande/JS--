2807b36af9a204487a9f7b3566c33fd09df892af
Vedika Gawande <vedikagawandde91@gmail.com>
2026-01-21 18:04:37 +0530

    Datatypes in Js

---
+++ JS/01_basics/02_Datatypes.js
@@ -0,0 +1,15 @@
+"use strict";//treat all JS code as newer version
+//alert(3+3) dont;beacuse we are using node
+
+let name="vedika" //String
+let age=19        // Number
+let isLoggedIn=false; //Boolean
+
+//Datatypes
+/*String,
+Numbers,
+Object,
+Undefined is the type,
+Null is the Object*/
+
+//Object
