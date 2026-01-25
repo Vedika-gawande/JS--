cbfcd0f08a9562ff4f5cfdef2c3f2d085aa888ee
Vedika Gawande <vedikagawandde91@gmail.com>
2026-01-25 11:59:18 +0530

    String methods in js

---
+++ JS/01_basics/06_String.js
@@ -0,0 +1,18 @@
+let Name = 'Vedika'
+let profession = 'Student'
+console.log(`My name is ${Name} and my profession is ${profession}`)
+
+const name='vedika-sg'
+console.log(name.charAt(3))
+console.log(name.indexOf('d'))
+console.log(name.length)
+console.log(name.toUpperCase())
+console.log(name.trim())
+
+const url='https://vedika20%gawande/.com'
+console.log(url.replace("20%",'-'))
+console.log(url.includes('vedika'))
+console.log(name.split('-'))
+console.log(name.split(''))
+console.log(name.slice(-5,3))
+console.log(name.substring(0,5))
