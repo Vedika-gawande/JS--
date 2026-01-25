fa1310488680946791f0a6ea1bcea7291ee82b8d
Vedika Gawande <vedikagawandde91@gmail.com>
2026-01-25 18:09:13 +0530

    Number and Maths in js

--- JS/01_basics/tempCodeRunnerFile.js
+++ JS/01_basics/tempCodeRunnerFile.js
@@ -1,8 +1,10 @@
-let value3=0
-// console.log(typeof value3)
-// let value4=false
-// console.log(typeof value4)
-// newValue3=Boolean(value3)
-// console.log(newValue3)
-// newValue4=Boolean(value3)
-// console.log(newValue4
+
+// const number = new Number(100)
+// console.log(number)
+// console.log(number.toString().length)
+const newNumber=123.45
+console.log(newNumber.toPrecision(3))
+const number3=100000
+console.log(number3.toLocaleString('en-In'))
+const number4=12.35
+console.log(number4.toFixed(3))
