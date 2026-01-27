894260100478a445f93dc6600cf20c7e97e72740
Vedika Gawande <vedikagawande91@gmail.com>
2026-01-27 18:00:01 +0530

    Array in js

---
+++ JS/01_basics/02_Basics/01_Array.js
@@ -0,0 +1,18 @@
+const myarr=[1,2,3,4,5,6]
+console.log(myarr)
+myarr.push(7)
+console.log(myarr)
+myarr.pop()
+console.log(myarr)
+myarr.unshift(0)
+console.log(myarr);
+myarr.shift()
+console.log(myarr)
+myarr.join()
+console.log(myarr)
+console.log(myarr)
+console.log(myarr.slice(1,4)) //doesnt change og array
+console.log(myarr.splice(1,4)) //change og array
+console.log(myarr)
+console.log(myarr.includes(5))
+console.log(myarr.indexOf(5))
