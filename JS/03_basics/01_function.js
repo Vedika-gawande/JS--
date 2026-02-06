faa6be33eca9ce448ff93febbaa9211990fcf50b
Vedika Gawande <vedikagawande91@gmail.com>
2026-02-06 12:00:55 +0530

    Functions in js

---
+++ JS/03_basics/01_function.js
@@ -0,0 +1,18 @@
+// default function
+// function add(){
+//     return 5+3
+// }
+// const add1 =add()
+// console.log(add1)
+
+// paramterized function
+// function mul(number1,number2){
+//     return number1*number2
+// }
+// console.log(mul(9,10))
+
+function isLogged(username){
+    return `${username} just logged in`
+}
+
+console.log(isLogged("vedika"))
