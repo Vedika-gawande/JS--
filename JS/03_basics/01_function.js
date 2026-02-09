14ffe8f0e1d35f72dcf20663253e478c88a670e4
Vedika Gawande <vedikagawande91@gmail.com>
2026-02-09 09:18:44 +0530

    Functions in js

--- JS/03_basics/01_function.js
+++ JS/03_basics/01_function.js
@@ -11,7 +11,30 @@
 // }
 // console.log(mul(9,10))
 
-function isLogged(username="bittu"){
-    return `${username} just logged in`
+// function isLogged(username="bittu"){
+//     return `${username} just logged in`
+// }
+// console.log(isLogged("vedika"))
+
+// const myObject={
+//     name:"vedika",
+//     rollno:18
+// }
+
+// function getObjectDetails(getObject){
+// console.log(`hello, my name is ${getObject.name} and rollno  is ${getObject.rollno}`)
+// }
+
+// getObjectDetails(myObject)
+
+// const myArray=[10,20,30]
+// function retrivemyArray(aryobj){
+//     return aryobj[1];
+// }
+// // console.log(retrivemyArray(myArray));
+// console.log(retrivemyArray([10,20,30,40]))
+
+function returnNum(val1,val2,...num){
+    return num;
 }
-console.log(isLogged("vedika"))
+console.log(returnNum(10,20,30,40))
